7bcb9172ee5acc03189431f46d0a9656e8fc096d
RogerNavelsaker <2011284+RogerNavelsaker@users.noreply.github.com>
2026-05-08 08:10:57 +0000

    chore: sync lean-ctx upstream

--- flake.nix
+++ flake.nix
@@ -13,8 +13,8 @@
         upstreamSrc = pkgs.fetchFromGitHub {
           owner = "yvgude";
           repo = "lean-ctx";
-          rev = "7a9633f4fb868bcfd820d42c8162726995794394";
-          hash = "sha256-gGu9c5ATWoG7Cb6Lz1mIYW1Ku9ps77K07lrAE5UP7FU=";
+          rev = "641cdd58a3b2db75b73e9d9e7f721348c9605b49";
+          hash = "sha256-ezlte66Y53vCyfU01KA/ZrlDj9qTjlUDKvN1dpJ7QOc=";
         };
         packageSrc = pkgs.runCommand "lean-ctx-3.2.8-src" { } ''
           mkdir -p "$out"
@@ -24,11 +24,11 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "lean-ctx";
-          version = "3.5.4";
+          version = "3.5.5";
 
           src = packageSrc;
 
-          cargoHash = "sha256-kIl3dSsN6WRoOnKrvsIo3DZeW2lTl7NPcC8AcxgVE9o=";
+          cargoHash = "sha256-NQlCADoUTr4dzUfIL3OjjNVYo0KyBlg+9w199INIJWI=";
 
           nativeBuildInputs = [ pkgs.pkg-config pkgs.perl ];
           buildInputs = [ pkgs.openssl pkgs.zlib ];
